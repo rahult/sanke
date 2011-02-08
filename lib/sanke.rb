451814f7da6b1c01871ea52817a1de9a369e799d
Rahul Trikha <rahul.trikha@gmail.com>
2011-02-08 23:55:32 +1030

    Added Images View

--- lib/sanke.rb
+++ lib/sanke.rb
@@ -1,17 +1,21 @@
-require "rubygems" if RUBY_VERSION < "1.9"
 require "sinatra/base"
-require "base64"
 require "digest/md5"
+require "base64"
 
 class Sanke < Sinatra::Base
   get '/' do
     'Welcome to sanka, to be used in conjuction with an iPhone!'
   end
 
+  get '/images' do
+    @images = Dir.glob("public/*.jpg")
+    erb :index, :views_directory => 'views'
+  end
+
   post '/store' do
     unless params[:data].empty?
       imageData = Base64.decode64(params[:data])
-      File.open("uploads/#{Digest::MD5.hexdigest(params[:data])}.jpg", 'w') { |f| f.write(imageData) }
+      File.open("public/#{Digest::MD5.hexdigest(params[:data])}.jpg", 'w') { |f| f.write(imageData) }
     end
   end
 
